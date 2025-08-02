949da1fc9e6f61b83f6ba32d47dfbc3813593532
MatiasFill <rmscontato@gmail.com>
2025-08-02 00:19:20 -0300

    clear all project and add configs to eslint in vscode file

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>
+  example
+</h1>
+</template>
+
+<script>
+export default {
+  name: 'ExampleComponent',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
